a042a6687f26569cbb9b0c031a9a259e6f70cc5e
Androidmate <59418150+Androidmate@users.noreply.github.com>
2020-01-02 01:16:14 +0530

    Add files via upload

---
+++ remove-adb.sh
@@ -0,0 +1,18 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+echo
+echo -e "\e[93mUninstalling Adb-tools-minimal"
+echo -e "\e[32m[*] \e[34mRemoving binaries..."
+rm -rf $PREFIX/bin/adb $PREFIX/bin/adb.bin $PREFIX/bin/fastboot $PREFIX/bin/fastboot-armeabi
+echo -e "\e[32m[*] \e[34mRemoving keys..."
+case `find $HOME/adbfiles -type f | wc -l` in
+0 | 2)
+  rm -rf $HOME/adbfiles ;;
+*)
+  rm -rf $HOME/adbfiles/adbkey $HOME/adbfiles/adbkey.pub ;;
+esac
+echo -e "\e[32m[*] \e[34mCleaning up..."
+rm -rf remove-adb.sh
+echo
+echo -e "\e[32mTools were successfully uninstalled!\e[39m"
+echo
