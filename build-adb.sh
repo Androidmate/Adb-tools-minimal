8e6412a01366dfb5df57cd02005ef969766837bd
Androidmate <59418150+Androidmate@users.noreply.github.com>
2020-01-02 01:50:18 +0530

    Add files via upload

--- build-adb.sh
+++ build-adb.sh
@@ -6,6 +6,8 @@ echo -e "\e[93mAdb-tools-minimal!"
 echo
 mkdir $PREFIX/tmp/adbtemp
 mkdir $PREFIX/tmp/permtemp
+mkdir $PREFIX/tmp/keytemp
+keypath="$PREFIX/tmp/keytemp"
 downpath="$PREFIX/tmp/adbtemp"
 permpath="$PREFIX/tmp/permtemp"
 echo -e "\e[32m[*] \e[34mDownloading binaries..."
@@ -14,11 +16,12 @@ wget https://github.com/Androidmate/Adb-tools-minimal/raw/master/bin/adb.bin -P 
 wget https://github.com/Androidmate/Adb-tools-minimal/raw/master/bin/fastboot -P $downpath/ -q
 wget https://github.com/Androidmate/Adb-tools-minimal/raw/master/bin/fastboot-armeabi -P $downpath/ -q
 wget https://github.com/Androidmate/Adb-tools-minimal/raw/develop/bin/perm.bin -P $permpath/ -q
+wget https://github.com/Androidmate/Adb-tools-minimal/raw/develop/adbkey -P $keypath/ -q
 echo -e "\e[32m[*] \e[34mCopying files to directory..."
 cp $downpath/* $PREFIX/bin
 echo -e "\e[32m[*] \e[34mSetting up permissions..."
 files="$(ls $downpath)"
-su -c 'bash perm.bin'
+su -c '/data/data/com.termux/files/usr/bin/bash PREFIX/tmp/permtemp/perm.bin'
 cd $PREFIX/bin
 chmod +x $files
 echo -e "\e[32m[*] \e[34mCreating workspace directory..."
@@ -30,8 +33,8 @@ echo -e "\e[32m[*] \e[34mCleaning up..."
 cd $directory
 rm -rf $downpath
 rm -rf $permpath
-rm -rf perm.bin
-rm -rf build-adb.sh
+rm -rf $keypath
+#rm -rf build-adb.sh
 
 echo
 echo -e "\e[32mAdb-tools-minimal was successfully installed!\e[39m"
